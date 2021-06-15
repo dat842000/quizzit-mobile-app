bdfcc1871c16a267b9f0effcb75bf0a65a745531
Đào Hữu Nghĩa <haseoleonard@gmail.com>
2021-06-15 21:37:36 +0700

    minor files added

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Laptrinh\AdroidStudioProject\Welcome-Login-Signup-Page-Flutter-master"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\haseo\Documents\quizzit-mobile-app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
