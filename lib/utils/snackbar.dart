b2c701c4b464488f3f221ac999a74ec4e00c037f
Bui Nguyen Hoang Long <bhoanglong@gmail.com>
2021-07-11 00:21:26 +0700

    fix lag dialog

--- lib/utils/snackbar.dart
+++ lib/utils/snackbar.dart
@@ -105,80 +105,34 @@ Widget _showLoadFlash(
   );
 }
 
-// void showLoadingFlash(
-//     {FlashBehavior style = FlashBehavior.floating,
-//     required BuildContext context,
-//     required Function(FlashController controller) onSuccess}) {
-//   showFlash(
-//     context: context,
-//     // duration: const Duration(seconds: 2),
-//     // persistent: true,
-//     builder: (ctx, controller) {
-//       onSuccess(controller);
-//       return Flash.bar(
-//         controller: controller,
-//         backgroundColor: Colors.white,
-//         brightness: Brightness.light,
-//         boxShadows: [BoxShadow(blurRadius: 4)],
-//         barrierBlur: 3.0,
-//         barrierColor: Colors.black38,
-//         barrierDismissible: true,
-//         behavior: style,
-//         position: FlashPosition.bottom,
-//         child: FlashBar(
-//           content: Text('Đang loading'),
-//           showProgressIndicator: true,
-//           primaryAction: TextButton(
-//             onPressed: () => controller.dismiss(),
-//             child: Text('Hủy', style: TextStyle(color: Colors.amber)),
-//           ),
-//         ),
-//       );
-//     },
-//   );
-// }
-
 void showDialogFlash(
     {required BuildContext context,
     required Function action,
     required String title}) {
-  showFlash(
-    context: context,
-    // duration: const Duration(seconds: 2),
-    persistent: true,
-    builder: (_, controller) {
-      return Flash.dialog(
-        controller: controller,
-        margin: const EdgeInsets.all(8),
-        borderRadius: const BorderRadius.all(Radius.circular(8)),
-        boxShadows: [BoxShadow(blurRadius: 4)],
-        barrierBlur: 3.0,
-        barrierColor: Colors.black38,
-        barrierDismissible: true,
-        child: FlashBar(
-          content: Text(
-            title,
-            style: TextStyle(fontSize: 20),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                controller.dismiss();
-              },
-              child: Text('Hủy',
-                  style: TextStyle(color: Colors.amber, fontSize: 16)),
-            ),
-            TextButton(
-              onPressed: () {
-                action();
-                controller.dismiss();
-              },
-              child: Text('OK',
-                  style: TextStyle(color: Colors.amber, fontSize: 16)),
-            ),
-          ],
-        ),
-      );
-    },
-  );
+  context.showFlashDialog(
+      margin: const EdgeInsets.all(8),
+      borderRadius: const BorderRadius.all(Radius.circular(8)),
+      boxShadows: [BoxShadow(blurRadius: 4)],
+      persistent: true,
+      // title: Text(""),
+      content: Text(
+        title,
+        style: TextStyle(fontSize: 20),
+      ),
+      negativeActionBuilder: (context, controller, _) {
+        return TextButton(
+          onPressed: () {
+            controller.dismiss();
+          },
+          child: Text('Hủy', style: TextStyle(color: Colors.amber, fontSize: 16)),
+        );
+      },
+      positiveActionBuilder: (context, controller, _) {
+        return TextButton(
+            onPressed: () {
+              action();
+              controller.dismiss();
+            },
+            child: Text('OK', style: TextStyle(color: Colors.amber, fontSize: 16)));
+      });
 }
